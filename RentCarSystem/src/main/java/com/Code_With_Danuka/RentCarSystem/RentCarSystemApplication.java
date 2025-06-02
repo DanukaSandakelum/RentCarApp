bc8238237ee8eed041fbdada5b99f72b5c5fbdf5
Danuka Sandakelum <sandakelumdanuka384@gmail.com>
2025-06-02 11:43:29 +0530

    create my project and connect database server

---
+++ RentCarSystem/src/main/java/com/Code_With_Danuka/RentCarSystem/RentCarSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Code_With_Danuka.RentCarSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentCarSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentCarSystemApplication.class, args);
+	}
+
+}
